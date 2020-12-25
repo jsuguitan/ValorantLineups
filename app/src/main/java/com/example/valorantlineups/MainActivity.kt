c3cb3e154716aea19a585e267d0c1de625a42739
Jervyn <jervyns@gmail.com>
2020-12-24 23:07:18 -0800

    24-Dec-20 Fixed Arrow to allow for video to show (Video has not been tested on Phone Emulator)

--- app/src/main/java/com/example/valorantlineups/MainActivity.kt
+++ app/src/main/java/com/example/valorantlineups/MainActivity.kt
@@ -1,40 +1,66 @@
 package com.example.valorantlineups
 
+
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.MediaController
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 
 class MainActivity : AppCompatActivity() {
 
-    var testVideo: VideoView? = null
-    var uri: Uri? = null
+    // declaring a null variable for VideoView
+    var simpleVideoView: VideoView? = null
+
+    // declaring a null variable for MediaController
+    var mediaControls: MediaController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        testVideo = (VideoView) findViewById(R.id.sovaVideoView);
-
-        uri = Uri.parse("android.resource://" + packageName() + "/" + R.raw.Sova_Haven_Attack_C)
 
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        // assigning id of VideoView from
+        // activity_main.xml layout file
+        simpleVideoView = findViewById<View>(R.id.sovaVideoView) as VideoView
+
+        if (mediaControls == null) {
+            // creating an object of media controller class
+            mediaControls = MediaController(this)
+
+            // set the anchor view for the video view
+            mediaControls!!.setAnchorView(this.simpleVideoView)
+        }
+
+        // set the media controller for video view
+        simpleVideoView!!.setMediaController(mediaControls)
+
+        // set the absolute path of the video file which is going to be played
+        simpleVideoView!!.setVideoURI(Uri.parse("android.resource://"
+                + packageName + "/" + R.raw.sova_haven_attack_c))
+
+        simpleVideoView!!.requestFocus()
+
+        // starting the video
+        simpleVideoView!!.start()
+
+        // display a toast message
+        // after the video is completed
+        simpleVideoView!!.setOnCompletionListener {
+            Toast.makeText(applicationContext, "Video completed",
+                Toast.LENGTH_LONG).show()
+        }
+
+        // display a toast message if any
+        // error occurs while playing the video
+        simpleVideoView!!.setOnErrorListener { mp, what, extra ->
+            Toast.makeText(applicationContext, "An Error Occured " +
+                    "While Playing Video !!!", Toast.LENGTH_LONG).show()
+            false
+        }
     }
 }
