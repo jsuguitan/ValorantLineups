5a43d7d8177dd48223a7fc7a12153974bea658a8
Jervyn <jervyns@gmail.com>
2020-12-29 10:37:47 -0800

    29-Dec-20 Added ScrollView for Buttons

--- app/src/main/java/com/example/valorantlineups/MapSelectionScreen.kt
+++ app/src/main/java/com/example/valorantlineups/MapSelectionScreen.kt
@@ -3,9 +3,9 @@ package com.example.valorantlineups
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class map_screen : AppCompatActivity() {
+class MapSelectionScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map_screen)
+        setContentView(R.layout.activity_map_selection_screen)
     }
 }
